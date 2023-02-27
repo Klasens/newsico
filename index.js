1dd82b3e1c2f2c1115c4e3acba1cd769f39483d8
Klasens <57231452+Klasens@users.noreply.github.com>
2023-02-27 15:31:07 -0600

    Added mobile menu design and functionality

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('#menu');
 const sideMenu = document.querySelector('#sideMenu');
+const sideMenuClose = document.querySelector('#menuClose');
 const pageOverlay = document.querySelector('#menuOverlay');
 const techInterviews = document.querySelector('#tech-interviews');
 const techInterviewsContent = document.querySelector(
@@ -13,7 +14,18 @@ const developerResourcesContent = document.querySelector(
 menu.addEventListener('click', function () {
   sideMenu.classList.add('opacity--1');
   pageOverlay.classList.add('opacity--1');
+  sideMenu.classList.remove('pointer-events--none');
   pageOverlay.classList.remove('pointer-events--none');
+});
+sideMenuClose.addEventListener('click', function () {
+  sideMenu.classList.remove('opacity--1');
+  pageOverlay.classList.remove('opacity--1');
+  techInterviewsContent.classList.remove('opacity--1');
+  developerResourcesContent.classList.remove('opacity--1');
+  developerResourcesContent.classList.add('pointer-events--none');
+  techInterviewsContent.classList.add('pointer-events--none');
+  pageOverlay.classList.add('pointer-events--none');
+  sideMenu.classList.add('pointer-events--none');
 });
 
 pageOverlay.addEventListener('click', function () {
@@ -22,13 +34,18 @@ pageOverlay.addEventListener('click', function () {
   techInterviewsContent.classList.remove('opacity--1');
   developerResourcesContent.classList.remove('opacity--1');
   pageOverlay.classList.add('pointer-events--none');
+  sideMenu.classList.add('pointer-events--none');
 });
 
 techInterviews.addEventListener('click', function () {
   techInterviewsContent.classList.add('opacity--1');
+  techInterviewsContent.classList.remove('pointer-events--none');
   developerResourcesContent.classList.remove('opacity--1');
+  developerResourcesContent.classList.add('pointer-events--none');
 });
 developerResources.addEventListener('click', function () {
   developerResourcesContent.classList.add('opacity--1');
+  developerResourcesContent.classList.remove('pointer-events--none');
   techInterviewsContent.classList.remove('opacity--1');
+  techInterviewsContent.classList.add('pointer-events--none');
 });
